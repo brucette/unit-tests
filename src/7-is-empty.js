abb834d8782c09cba09874e949da7b6ca1175abc
Tina Bruce <tinabruce42@hotmail.com>
2022-11-18 09:59:30 +0100

    Tests 1-7 done

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -4,6 +4,11 @@ export const isEmpty = (stringArrayOrObject) => {
   if (type === 'string') {
     return stringArrayOrObject === ''
   }
-
-  return false
+  else if (type === 'array') {
+    return stringArrayOrObject.length === 0
+  }
+  else if (type === 'object') {
+    return Object.keys(stringArrayOrObject).length === 0
+  }
+  else return false
 }
