2e21491073e0ac2f077eed88f686c6c9726a0005
Tina Bruce <tinabruce42@hotmail.com>
2023-03-03 14:54:17 +0100

    Completed tests 8-9

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,23 @@
 export const hackerSpeak = (words) => {
-  return words
+  const as = /[aA]/ig;
+  const es = /[eE]/ig;
+  const is = /[iI]/ig;
+  const os = /[oO]/ig;
+  const ss = /[sS]/ig;
+
+  const replaceables = [
+    {letter: as, number: 4}, 
+    {letter: es, number: 3}, 
+    {letter: is, number: 1}, 
+    {letter: os, number: 0}, 
+    {letter: ss, number: 5}
+  ]
+
+  let hackered = words;
+
+  for (const item of replaceables) {
+    hackered = hackered.replaceAll(item.letter, item.number)
+  }
+
+  return hackered
 }
